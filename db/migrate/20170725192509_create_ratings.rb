116e1b8f1dbb84e676882ef00780a74321082b8e
Hans-Sebastian <hanssebastian.p@gmail.com>
2017-07-25 15:36:46 -0400

    scaffolding

---
+++ db/migrate/20170725192509_create_ratings.rb
@@ -0,0 +1,13 @@
+class CreateRatings < ActiveRecord::Migration[5.1]
+  def change
+    create_table :ratings do |t|
+      t.integer :bar_id
+      t.integer :user_id
+      t.integer :rating
+      t.belongs_to :user, foreign_key: true
+      t.belongs_to :bar, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
---
+++ db/migrate/20170725193527_create_bars.rb
@@ -0,0 +1,26 @@
+class CreateBars < ActiveRecord::Migration[5.1]
+  def change
+    create_table :bars do |t|
+      t.string :name
+      t.string :address
+      t.string :phone_number
+      t.integer :avg_rating
+      t.integer :rating_id
+      t.boolean :music
+      t.boolean :sports
+      t.boolean :dancing
+      t.boolean :beer
+      t.boolean :cocktails
+      t.boolean :wine
+      t.boolean :rooftop
+      t.boolean :cheap_food
+      t.integer :food
+      t.boolean :dress_code
+      t.boolean :line
+      t.integer :popular_nights
+      t.boolean :karaoke
+
+      t.timestamps
+    end
+  end
+end
