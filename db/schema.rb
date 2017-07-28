de156ac326d93580bc4c52fc9274304e72f04445
ErinKarlaYoung <erinkarlayoung@gmail.com>
2017-07-28 17:49:18 -0400

    SVG file

--- db/schema.rb
+++ db/schema.rb
@@ -55,7 +55,6 @@ ActiveRecord::Schema.define(version: 20170728150005) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "username"
     t.boolean "admin"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
